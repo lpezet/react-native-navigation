15fa1ba9c5eca50aba67c5f3be885a87ce30ff32
Luke Pezet <lpezet@gmail.com>
2020-03-15 16:55:55 -0600

    Navigation with params strongly typed.

--- App.tsx
+++ App.tsx
@@ -1,37 +1,77 @@
 import React from "react";
 import "react-native-gesture-handler";
-import { NavigationContainer, ParamListBase } from "@react-navigation/native";
+import { NavigationContainer, RouteProp } from "@react-navigation/native";
 import {
   StackNavigationProp,
   createStackNavigator
 } from "@react-navigation/stack";
 import { Button, Text, View } from "react-native";
 
-interface Props {
-  navigation: StackNavigationProp<ParamListBase, "Home">;
+type RootStackParamList = {
+  Home: {};
+  Details: {
+    itemId: number;
+    otherParam?: string;
+  };
+};
+
+interface ScreenProps<RouteName extends keyof RootStackParamList> {
+  navigation: StackNavigationProp<RootStackParamList, RouteName>;
+  route: RouteProp<RootStackParamList, RouteName>;
 }
 
-function HomeScreen({ navigation }: Props) {
+function HomeScreen({ navigation }: ScreenProps<"Home">) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
       <Button
         title="Go to Details"
-        onPress={() => navigation.navigate("Details")}
+        onPress={() => {
+          navigation.navigate("Details", {
+            itemId: 86,
+            otherParam: "anything you want here"
+          });
+        }}
       />
     </View>
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({
+  route: { params },
+  navigation
+}: ScreenProps<"Details">) {
+  const { itemId } = params;
+  const { otherParam } = params;
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Details Screen</Text>
+      <Text>
+        itemId:
+        {JSON.stringify(itemId)}
+      </Text>
+      <Text>
+        otherParam:
+        {JSON.stringify(otherParam)}
+      </Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() =>
+          navigation.push("Details", {
+            itemId: Math.floor(Math.random() * 100)
+          })
+        }
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
     </View>
   );
 }
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
