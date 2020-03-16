44d7061a89247a9388f76eb5f82957114f04020c
Luke Pezet <lpezet@gmail.com>
2020-03-15 21:03:08 -0600

    Custom back button added.

--- App.tsx
+++ App.tsx
@@ -5,7 +5,14 @@ import {
   StackNavigationProp,
   createStackNavigator
 } from "@react-navigation/stack";
-import { Button, Image, Text, View } from "react-native";
+import {
+  Button,
+  Image,
+  ImageStyle,
+  StyleSheet,
+  Text,
+  View
+} from "react-native";
 
 type RootStackParamList = {
   Home: {};
@@ -13,7 +20,9 @@ type RootStackParamList = {
     name: string;
   };
   CustomHeader: {};
+  CustomHeader2: {};
   LogoTitle: {};
+  CustomBackButton: {};
 };
 
 interface ScreenProps<RouteName extends keyof RootStackParamList> {
@@ -39,6 +48,38 @@ function CustomHeaderScreen({ navigation }: ScreenProps<"CustomHeader">) {
   );
 }
 
+function CustomBackButtonScreen({
+  navigation
+}: ScreenProps<"CustomBackButton">) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Custom Header (with a logo)</Text>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+function CustomHeader2Screen({ navigation }: ScreenProps<"CustomHeader2">) {
+  const [count, setCount] = React.useState(0);
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Button onPress={() => setCount(c => c + 1)} title="Update count" />
+      )
+    });
+  }, [navigation, setCount]);
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>
+        Count:
+        {count}
+      </Text>
+    </View>
+  );
+}
+
 function HomeScreen({ navigation }: ScreenProps<"Home">) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
@@ -58,11 +99,49 @@ function HomeScreen({ navigation }: ScreenProps<"Home">) {
           navigation.navigate("CustomHeader");
         }}
       />
+      <Button
+        title="Go to CustomerHeader 2 Screen"
+        onPress={() => {
+          navigation.navigate("CustomHeader2");
+        }}
+      />
+      <Button
+        title="Go to Custom Back Button Screen"
+        onPress={() => {
+          navigation.navigate("CustomBackButton");
+        }}
+      />
     </View>
   );
 }
 
 const Stack = createStackNavigator<RootStackParamList>();
+
+const styles = StyleSheet.create({
+  myCustomHeaderBackImage: {
+    height: 14.5,
+    width: 24,
+    marginLeft: 9,
+    marginRight: 12,
+    marginVertical: 12,
+    resizeMode: "contain"
+  }
+  /*
+  myCustomHeaderBackImageAlt: {
+    tintColor: "#f00"
+  }
+  */
+});
+
+function MyCustomHeaderBackImage(style: ImageStyle) {
+  return (
+    <Image
+      source={require("./assets/back.png")}
+      // , this.props.style
+      style={[styles.myCustomHeaderBackImage, style]}
+    />
+  );
+}
 
 function LogoTitleHeader() {
   return (
@@ -103,8 +182,38 @@ export default function App() {
           component={CustomHeaderScreen}
           // not sure what type "props" would be here:
           // headerTitle: props => <LogoTitleHeader {...props} />
-          options={{ headerTitle: () => <LogoTitleHeader /> }}
+          options={{
+            headerTitle: () => <LogoTitleHeader />,
+            headerRight: () => (
+              <Button
+                onPress={() => {
+                  console.log("This is a button!");
+                }}
+                title="Info"
+              />
+            )
+          }}
         />
+        <Stack.Screen
+          name="CustomHeader2"
+          component={CustomHeader2Screen}
+          // not sure what type "props" would be here:
+          // headerTitle: props => <LogoTitleHeader {...props} />
+          options={{
+            headerTitle: () => <LogoTitleHeader />
+          }}
+        />
+        <Stack.Screen
+          name="CustomBackButton"
+          component={CustomBackButtonScreen}
+          // not sure what type "props" would be here:
+          // headerTitle: props => <LogoTitleHeader {...props} />
+          options={{
+            // style={styles.myCustomHeaderBackImageAlt}
+            headerBackImage: () => <MyCustomHeaderBackImage />
+          }}
+        />
+
         <Stack.Screen
           name="Profile"
           component={ProfileScreen}
