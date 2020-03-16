ddcfeeb42868ab49354ab33d833233b67ea2365c
Luke Pezet <lpezet@gmail.com>
2020-03-15 22:43:09 -0600

    Use of React useRef() and navigation container.

--- App.tsx
+++ App.tsx
@@ -1,76 +1,54 @@
 import React from "react";
 import "react-native-gesture-handler";
-import { NavigationContainer, RouteProp } from "@react-navigation/native";
 import {
-  StackNavigationProp,
-  createStackNavigator
-} from "@react-navigation/stack";
+  NavigationContainer,
+  NavigationContainerRef
+} from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { Button, Text, View } from "react-native";
 
 type RootStackParamList = {
   Home: {};
-  MyModal: {};
   Details: {};
 };
 
+/*
 interface ScreenProps<RouteName extends keyof RootStackParamList> {
   navigation: StackNavigationProp<RootStackParamList, RouteName>;
   route: RouteProp<RootStackParamList, RouteName>;
 }
+*/
 
-function HomeScreen({ navigation }: ScreenProps<"Home">) {
+const Stack = createStackNavigator<RootStackParamList>();
+
+function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 30 }}>This is the home screen!</Text>
-      <Button
-        onPress={() => navigation.navigate("MyModal")}
-        title="Open Modal"
-      />
-      <Button
-        onPress={() => navigation.navigate("Details")}
-        title="Go to Details"
-      />
+      <Text>Home Screen</Text>
     </View>
   );
 }
-
-function ModalScreen({ navigation }: ScreenProps<"Home">) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 30 }}>This is a modal!</Text>
-      <Button onPress={() => navigation.goBack()} title="Dismiss" />
-    </View>
-  );
-}
-
 function DetailsScreen() {
   return (
-    <View>
-      <Text>Details</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
     </View>
-  );
-}
-
-const MainStack = createStackNavigator();
-const RootStack = createStackNavigator();
-
-function MainStackScreen() {
-  return (
-    <MainStack.Navigator>
-      <MainStack.Screen name="Home" component={HomeScreen} />
-      <MainStack.Screen name="Details" component={DetailsScreen} />
-    </MainStack.Navigator>
   );
 }
 
 function App() {
+  const ref = React.useRef<NavigationContainerRef>(null);
+
   return (
-    <NavigationContainer>
-      <RootStack.Navigator mode="modal" headerMode="none">
-        <RootStack.Screen name="Main" component={MainStackScreen} />
-        <RootStack.Screen name="MyModal" component={ModalScreen} />
-      </RootStack.Navigator>
-    </NavigationContainer>
+    <View style={{ flex: 1 }}>
+      <NavigationContainer ref={ref}>
+        <Stack.Navigator initialRouteName="Details">
+          <Stack.Screen name="Details" component={DetailsScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <Button onPress={() => ref.current?.navigate("Home")} title="Go home" />
+    </View>
   );
 }
 
